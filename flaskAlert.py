a9ce7b93cd2ae3149156fe45d0f548731a22f0ae
Vladimir Kamarzin <vvk@vvk.pp.ru>
2018-06-12 23:32:21 +0500

    Refactor app to be cleaner
    Used ideas from https://github.com/count0ru/tg-alertbot

--- flaskAlert.py
+++ flaskAlert.py
@@ -1,7 +1,9 @@
-from flask import Flask
-from flask import request
 import telegram
 import logging
+
+from flask import Flask
+from flask import request
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -18,27 +20,23 @@ def postAlertmanager():
 
     content = request.get_json()
 
-    try:
+    if not content:
+        abort(400)
 
-        for alert in content['alerts']:
+    alert_name = content['alerts'][0]['labels']['alertname']
+    # just strip microseconds part
+    d = content['alerts'][0]['startsAt'].split('.')[0]
+    alert_time = str(datetime.strptime(d, '%Y-%m-%dT%H:%M:%S'))
+    alert_high = content['alerts'][0]['labels']['severity'].upper()
+    alert_inst = content['alerts'][0]['labels']['instance']
+    # assume desciption may be empty
+    alert_desc = content['alerts'][0]['annotations'].get('description', None)
 
-            message = """
-Status """+alert['status']+"""
-    
-Alertname: """+alert['labels']['alertname']+"""
+    message = '[{}] {} at {}\non {} \n{}'.format(
+            alert_high, alert_name, alert_time, alert_inst, alert_desc)
+    bot.sendMessage(chat_id=CHAT_ID, text=message)
 
-Instance: """+alert['labels']['instance']+"""("""+alert['labels']['name']+""")
-    
-"""+alert['annotations']['description']+"""
-"""
-            bot.sendMessage(chat_id=CHAT_ID, text=message)
-    except Exception as e:
-
-        logger.info(content)
-        bot.sendMessage(chat_id=CHAT_ID, text=e)
-
-    return
+    return ''
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='flaskAlert.log',level=logging.INFO)
     app.run(host='0.0.0.0', port=9119)
