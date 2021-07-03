f44be590748ad0e54af79adaf26586acb173376d
Vladimir Kamarzin <vvk@vvk.pp.ru>
2021-07-03 15:49:08 +0500

    Instance label can be missing

--- flaskAlert.py
+++ flaskAlert.py
@@ -8,14 +8,15 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-app.secret_key = 'aYT>.L$kk2h>!'
+app.secret_key = "aYT>.L$kk2h>!"
 
-CHAT_ID = 'chatID'
-TOKEN = 'botToken'
+CHAT_ID = "chatID"
+TOKEN = "botToken"
 
 bot = telegram.Bot(token=TOKEN)
 
-@app.route('/alert', methods = ['POST'])
+
+@app.route("/alert", methods=["POST"])
 def postAlertmanager():
 
     content = request.get_json()
@@ -23,22 +24,27 @@ def postAlertmanager():
     if not content:
         abort(400)
 
-    alert_name = content['alerts'][0]['labels']['alertname']
+    alert = content["alerts"][0]
+    alert_name = alert["labels"]["alertname"]
     # just strip microseconds part
-    d = content['alerts'][0]['startsAt'].split('.')[0]
-    alert_time = str(datetime.strptime(d, '%Y-%m-%dT%H:%M:%S'))
-    alert_high = content['alerts'][0]['labels']['severity'].upper()
-    alert_inst = content['alerts'][0]['labels']['instance']
+    d = alert["startsAt"].split(".")[0]
+    alert_time = str(datetime.strptime(d, "%Y-%m-%dT%H:%M:%S"))
+    alert_high = alert["labels"]["severity"].upper()
+    alert_inst = alert["labels"].get("instance")
     # assume desciption may be empty
 
-    message = '[{}] {} at {}\non {}\n'.format(
-            alert_high, alert_name, alert_time, alert_inst)
+    message = "[{}] {} at {}\non {}\n".format(
+        alert_high, alert_name, alert_time, alert_inst
+    )
 
-    for annotation in content['alerts'][0]['annotations']:
-        message += '{}: {}\n'.format(annotation, content['alerts'][0]['annotations'][annotation])
+    for annotation in alert["annotations"]:
+        message += "{}: {}\n".format(
+            annotation, alert["annotations"][annotation]
+        )
     bot.sendMessage(chat_id=CHAT_ID, text=message)
 
-    return ''
+    return ""
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9119)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9119)
